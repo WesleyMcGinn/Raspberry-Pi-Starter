89c6d5c6cd48be21c7de199ac2f98ac2ef9bcc31
Wesley <wesleymcginn1@gmail.com>
2026-03-03 23:47:52 -0500

    Fix pigpio bug

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ sudo apt remove --purge -y rpi-connect
 sudo apt remove --purge -y rpi-connect-lite
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y nodejs npm tmux
+sudo apt install -y pigpio nodejs npm tmux
 sudo apt autoremove -y
 sudo apt clean
 npm i ws shelljs pigpio
